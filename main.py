c69600754c3738a70d997d8b5dc10360d26b17e1
Jwix <macs30master@gmail.com>
2023-07-11 10:43:46 +0300

    Refactoring

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-'''ПРОГРАММА ИЗ ТГ'''
-print("start")
+
 import random
 
-x = [random.randint(1, 100) for i in range(100)]
+x = [random.randint(1, 999) for i in range(100)]
 
 for i in range(len(x)-1):
     if x[i] > x[i+1]:
         print(f"{x[i]}(сосед {x[i+1]})")
-
-print("Finish")
